751b684a5d5df9a655f6e94d001df07ebb7ae490
Parshant Singh <parshant.e2@gmail.com>
2024-09-10 19:08:02 +0100

    Added sentry

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,18 @@
 use clap::Parser;
 use dwallet_gas_pool::command::Command;
 
-#[tokio::main]
-async fn main() {
-    print!("hi");
+fn main() {
+    let _guard = sentry::init(("https://1dd0fd2be29f39361bf956ece4336d25@o4507907608150016.ingest.de.sentry.io/4507929902841936", sentry::ClientOptions {
+        release: sentry::release_name!(),
+        ..Default::default()
+      }));
 
-    let command = Command::parse();
-    command.execute().await;
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            let command = Command::parse();
+            command.execute().await;
+        });
 }
