a1c9bced7c5ed5cfcef5089ef209edc83616f854
Parshant Singh <parshant.e2@gmail.com>
2024-11-17 21:07:48 +0000

    add script

---
+++ run_dwallet_gas_pool.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Start haproxy in background
+haproxy -f /etc/haproxy/haproxy.cfg
+
+# Start promtail in background
+/usr/local/bin/promtail -config.file=/etc/promtail/config.yml &
+
+# Start dwallet gas pool
+/usr/local/bin/dwallet_gas_pool --config-path /usr/local/bin/testnet-prod.yaml
