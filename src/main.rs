ca02884da1c826fce7ce2c6e283940ce888ca391
Parshant Singh <parshant.e2@gmail.com>
2024-12-09 00:01:07 +0000

    Disable sentry alerts in dev

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,21 @@
 
 use clap::Parser;
 use dwallet_gas_pool::command::Command;
+use sentry;
+use std::env;
+use std::{marker::PhantomData, str::FromStr, sync::Arc};
 
 fn main() {
     let _guard = sentry::init(("https://1dd0fd2be29f39361bf956ece4336d25@o4507907608150016.ingest.de.sentry.io/4507929902841936", sentry::ClientOptions {
         release: sentry::release_name!(),
         attach_stacktrace: true,
+        environment: Some(env::var("NODE_ENV").expect("NODE_ENV not found in environment variables").into()),
+        before_send: Some(Arc::new(|event| {
+            if event.environment.as_deref() == Some("development") {
+                return None;
+            }
+            Some(event)
+        })),
         ..Default::default()
       }));
 
